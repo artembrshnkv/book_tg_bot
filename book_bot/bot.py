48a85acc6481d42797d5d11ebc215158dcb3a20f
ArtemBaryshnikov <baryshnikov-artem@inbox.ru>
2023-07-17 14:39:43 +0300

    Delete old replykb and menu

--- book_bot/bot.py
+++ book_bot/bot.py
@@ -2,6 +2,7 @@ import asyncio
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
+from aiogram.types import ReplyKeyboardRemove
 
 import config as cfg
 
@@ -12,6 +13,18 @@ dp = Dispatcher()
 @dp.message(Command('start'))
 async def start_command_handler(message: types.Message):
     await message.answer('Добро пожаловать в бот!')
+
+
+@dp.message(Command('delmenu'))
+async def del_main_menu(message: types.Message):
+    await bot.delete_my_commands()
+    await message.answer(text='Меню успешно удалено')
+
+
+@dp.message(Command('delreplykb'))
+async def del_reply_keyboard(message: types.Message):
+    await message.answer(text='Клавиатура успешно удалена', reply_markup=ReplyKeyboardRemove())
+
 
 
 async def main():
