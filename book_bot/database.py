3caeb33157690651c2c39d12d718be38c57bbf76
ArtemBaryshnikov <baryshnikov-artem@inbox.ru>
2023-07-18 15:31:56 +0300

    Add books adding, and checking if it is already in table mechanic

--- book_bot/database.py
+++ book_bot/database.py
@@ -5,24 +5,26 @@ import config as cfg
 conn = psql.connect(dbname=cfg.DATABASE_NAME, port=cfg.DB_PORT, host=cfg.DB_HOST,
                     user=cfg.DB_USER, password=cfg.DB_PASSWORD)
 
-with conn.cursor() as cur:
-    try:
+
+# Checking if the book in table already
+def _already_in_table(title):
+    with conn.cursor() as cur:
         cur.execute("""
-        CREATE TABLE IF NOT EXISTS books(
-        id SERIAL PRIMARY KEY,
-        title VARCHAR(200)
-        )
-        """)
-        cur.execute("""
-        CREATE TABLE IF NOT EXISTS pages(
-        id SERIAL PRIMARY KEY,
-        content text,
-        book_id INTEGER REFERENCES books (id) ON DELETE RESTRICT
-        )""")
-        conn.commit()
-        print('Tables created successfully')
-    except:
-        print('Error')
+        SELECT * FROM books WHERE title = %s
+        """, (title, ))
+        return cur.fetchone()
 
 
-
+# Adds book to the table, raise exception if it in already
+def add_book(title):
+    if not _already_in_table(title):
+        with conn.cursor() as cur:
+            cur.execute("""
+            INSERT INTO books (title) 
+            VALUES (%(title)s)
+            """,
+                        {'title': title})
+            conn.commit()
+            print('Book added successfully')
+    else:
+        print('Book already in table')
--- book_bot/utils.py
+++ book_bot/utils.py
@@ -1,14 +1,16 @@
 import config as cfg
+import database as db
 
-file = 'books/selindzher-d.-d.-nad-propastyu-vo-rzhi-getlib.ru.pdf'
+file = 'books/Над_пропасть_во_ржи.pdf'
 punctuation_marks = ('.', ',', '!', '?', ':', ';')
 
 
-def get_parts():
-    with open(file, 'r') as f:
-        while f >= cfg.PAGE_SIZE:
-            print(f.tell())
+db.add_book(file[6:-4])
+
+# def get_parts():
+#     with open(file, 'r') as f:
+#         while f >= cfg.PAGE_SIZE:
+#             print(f.tell())
 
 
-get_parts()
 
