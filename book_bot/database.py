2060e7d446d8277ae3e47e0092ff1d6a2b60f31b
ArtemBaryshnikov <baryshnikov-artem@inbox.ru>
2023-07-18 20:20:24 +0300

    Reformat

--- book_bot/database.py
+++ book_bot/database.py
@@ -6,18 +6,53 @@ conn = psql.connect(dbname=cfg.DATABASE_NAME, port=cfg.DB_PORT, host=cfg.DB_HOST
                     user=cfg.DB_USER, password=cfg.DB_PASSWORD)
 
 
-# Checks if the book in table already
-def _already_in_table(title):
-    with conn.cursor() as cur:
-        cur.execute("""
-        SELECT * FROM books WHERE title = %s
-        """, (title, ))
-        return cur.fetchone()
+# # Checks if the book in table already
+# def _already_in_table(title):
+#     with conn.cursor() as cur:
+#         cur.execute("""
+#         SELECT * FROM books WHERE title = %s
+#         """, (title, ))
+#         return cur.fetchone()
+#
+#
+# # Gets book id by title
+# def gget_book_id_by_title(title):
+#     try:
+#         with conn.cursor() as cur:
+#             cur.execute("""
+#             SELECT * FROM books WHERE title = %s
+#             """, (title, ))
+#             return cur.fetchone()[0]
+#     except:
+#         print('No books with this title')
 
 
-# Adds book to the table, raise exception if it in already
+def select_book(title, already_in_table=False, get_book_id_by_title=False):
+    """Select book from books table.
+     If already_in_book = True returns boolean if book in table.
+     If get_book_id_by_title = True returns book id.
+     If both False or both True returns None
+     """
+    try:
+        with conn.cursor() as cur:
+            cur.execute("""
+            SELECT * FROM books WHERE title = %s 
+            """, (title,))
+            if already_in_table and get_book_id_by_title or \
+                    not already_in_table and not get_book_id_by_title:
+                return None
+            elif already_in_table:
+                return bool(cur.fetchone())
+            elif get_book_id_by_title:
+                return cur.fetchone()[0]
+    except:
+        print('No book with this title')
+
+
+#
 def add_book(title):
-    if not _already_in_table(title):
+    """Adds book to the table, raise exception if it in already"""
+    if not select_book(title, already_in_table=True):
         with conn.cursor() as cur:
             cur.execute("""
             INSERT INTO books (title) 
@@ -30,12 +65,12 @@ def add_book(title):
         print('Book already in table')
 
 
-# Adds book pages
+#
 def add_pages(content, book_id):
+    """Adds book pages"""
     with conn.cursor() as cur:
         cur.execute("""
         INSERT INTO pages (content, book_id) VALUES (%(content)s, %(book_id)s)
         """, {'content': content, 'book_id': book_id})
         conn.commit()
         print('Page(s) successfully added')
-
--- book_bot/utils.py
+++ book_bot/utils.py
@@ -5,12 +5,12 @@ file = 'books/Над_пропасть_во_ржи.pdf'
 book_title = file[6:-4]
 punctuation_marks = ('.', ',', '!', '?', ':', ';')
 
+print(type(db.select_book(book_title, already_in_table=True)))
 
 
-# def get_parts():
+# def get_pages():
 #     with open(file, 'r') as f:
 #         while f >= cfg.PAGE_SIZE:
 #             print(f.tell())
-
 
 
